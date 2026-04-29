7db2ae43d33c6bce2c9dec28c7edcaa8759ef93e
f-giacobbe <100165496+f-giacobbe@users.noreply.github.com>
2026-04-29 13:13:52 +0200

    Mcdonalds crew

--- mcdonalds_crew/src/mcdonalds_crew/main.py
+++ mcdonalds_crew/src/mcdonalds_crew/main.py
@@ -4,7 +4,7 @@ import warnings
 
 from datetime import datetime
 
-from thesis.crew import Thesis
+from mcdonalds_crew.crew import McdonaldsCrew
 
 warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
 
@@ -23,7 +23,7 @@ def run():
     }
 
     try:
-        Thesis().crew().kickoff(inputs=inputs)
+        McdonaldsCrew().crew().kickoff(inputs=inputs)
     except Exception as e:
         raise Exception(f"An error occurred while running the crew: {e}")
 
@@ -37,7 +37,7 @@ def train():
         'current_year': str(datetime.now().year)
     }
     try:
-        Thesis().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
+        McdonaldsCrew().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
 
     except Exception as e:
         raise Exception(f"An error occurred while training the crew: {e}")
@@ -47,7 +47,7 @@ def replay():
     Replay the crew execution from a specific task.
     """
     try:
-        Thesis().crew().replay(task_id=sys.argv[1])
+        McdonaldsCrew().crew().replay(task_id=sys.argv[1])
 
     except Exception as e:
         raise Exception(f"An error occurred while replaying the crew: {e}")
@@ -62,7 +62,7 @@ def test():
     }
 
     try:
-        Thesis().crew().test(n_iterations=int(sys.argv[1]), eval_llm=sys.argv[2], inputs=inputs)
+        McdonaldsCrew().crew().test(n_iterations=int(sys.argv[1]), eval_llm=sys.argv[2], inputs=inputs)
 
     except Exception as e:
         raise Exception(f"An error occurred while testing the crew: {e}")
@@ -88,7 +88,7 @@ def run_with_trigger():
     }
 
     try:
-        result = Thesis().crew().kickoff(inputs=inputs)
+        result = McdonaldsCrew().crew().kickoff(inputs=inputs)
         return result
     except Exception as e:
         raise Exception(f"An error occurred while running the crew with trigger: {e}")
